84372baadba7052887d4847ac52207ed8b8ecc2f
ThisizHTZ <141153854+ThisizHTZ@users.noreply.github.com>
2024-01-19 20:36:31 -0800

    StringUtils.cpp

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -53,38 +53,71 @@ std::string Center(const std::string &str, int width, char fill) noexcept{
 }
 
 std::string LJust(const std::string &str, int width, char fill) noexcept{
-    // Replace code here
-    return "";
+    if (str.length() >= width) return str;
+    return str + std::string(width - str.length(), fill);
 }
 
 std::string RJust(const std::string &str, int width, char fill) noexcept{
-    // Replace code here
-    return "";
+    size_t end = str.find_last_not_of(" \\t\\n\\r\\f\\v");
+    return (end == std::string::npos) ? "" : str.substr(0, end + 1);
 }
 
 std::string Replace(const std::string &str, const std::string &old, const std::string &rep) noexcept{
-    // Replace code here
-    return "";
+    if (str.length() >= width) return str;
+    return std::string(width - str.length(), fill) + str;
 }
 
 std::vector< std::string > Split(const std::string &str, const std::string &splt) noexcept{
-    // Replace code here
-    return {};
+    std::vector<std::string> tokens;
+    size_t start = 0, end = 0;
+    while ((end = str.find(delim, start)) != std::string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + delim.length();
+    }
+    tokens.push_back(str.substr(start));
+    return tokens;
 }
 
 std::string Join(const std::string &str, const std::vector< std::string > &vect) noexcept{
-    // Replace code here
-    return "";
+    std::ostringstream oss;
+    for (size_t i = 0; i < vect.size(); ++i) {
+        if (i != 0) oss << delim;
+        oss << vect[i];
+    }
+    return oss.str();
 }
 
 std::string ExpandTabs(const std::string &str, int tabsize) noexcept{
-    // Replace code here
-    return "";
+    std::string result;
+    for (char ch : str) {
+        if (ch == '\\t') {
+            result += std::string(tabsize, ' ');
+        } else {
+            result += ch;
+        }
+    }
+    return result;
 }
 
 int EditDistance(const std::string &left, const std::string &right, bool ignorecase) noexcept{
-    // Replace code here
-    return 0;
+    std::string l = ignorecase ? Lower(left) : left;
+    std::string r = ignorecase ? Lower(right) : right;
+    std::vector<std::vector<int>> dp(l.size() + 1, std::vector<int>(r.size() + 1));
+
+    for (size_t i = 0; i <= l.size(); ++i) {
+        for (size_t j = 0; j <= r.size(); ++j) {
+            if (i == 0) dp[i][j] = j;
+            else if (j == 0) dp[i][j] = i;
+            else if (l[i - 1] == r[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+            else {
+                dp[i][j] = 1 + std::min({dp[i - 1][j],    // Insert
+                                         dp[i][j - 1],    // Remove
+                                         dp[i - 1][j - 1] // Replace
+                                        });
+            }
+        }
+    }
+    return dp[l.size()][r.size()];
 }
 
 };
